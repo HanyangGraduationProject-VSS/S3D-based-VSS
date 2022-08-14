b1a68517d4cb90e92afd694afc5fc3f7599edb81
BraveSejin <ihavebravebiz@gmail.com>
2022-08-14 17:44:49 +0900

    update

--- torch_dataset_generator.py
+++ torch_dataset_generator.py
@@ -6,11 +6,13 @@ from features_logits_loader import load_feature_map_and_logits
 from utils import checkParquetExist
 import argparse
 
+import numpy as np
+
 
 class WindowState(Enum):
-    START = 1
-    END = 2
-    NONE = 3
+    START = 0
+    END = 1
+    NONE = 2
 
 
 class LogitDataset(Dataset):
@@ -44,7 +46,7 @@ class DatasetGenerator():
         self.segment_table = convert_seconds_to_frame_indices_in_segments()
         self.video_ids = self.segment_table.index.unique()
 
-    def generateFeatureMapDataset(self,num_to_gen):
+    def generateFeatureMapDataset(self,num_to_gen, num_windows = 1):
         print("##generating logit dataset##")
         feature_maps, states = [], []
         for idx, video_id in enumerate(self.video_ids[:num_to_gen]):
@@ -53,11 +55,14 @@ class DatasetGenerator():
                 continue
             feature_maps_states = self.videos_to_feature_maps_states(
                 "v_"+video_id)
-            feature_maps += feature_maps_states[0]
-            states += feature_maps_states[1]
+            total_frames = len(feature_maps_states[1])
+            indices = np.arange(total_frames)
+            indices_to_fetch = np.array([np.clip(np.arange(index - num_windows // 2, index + num_windows // 2 + 1), 0, total_frames - 1) for index in indices])
+            feature_maps += [*np.array(feature_maps_states[0])[indices_to_fetch]]
+            states += [*np.array(list(map(lambda s: s.value, feature_maps_states[1])))[indices_to_fetch]]
         return FeatureMapDataset(feature_maps, states)
 
-    def generateLogitDataset(self,num_to_gen):
+    def generateLogitDataset(self,num_to_gen, num_windows = 1):
         print("##generating logit dataset##")
         logits, states = [], []
         for idx, video_id in enumerate(self.video_ids[:num_to_gen]):
@@ -65,8 +70,11 @@ class DatasetGenerator():
             if not checkParquetExist("v_"+video_id): 
                 continue
             logits_states = self.videos_to_logits_states("v_"+video_id)
-            logits += logits_states[0]
-            states += logits_states[1]
+            total_frames = len(logits_states[1])
+            indices = np.arange(total_frames)
+            indices_to_fetch = np.array([np.clip(np.arange(index - num_windows // 2, index + num_windows // 2 + 1), 0, total_frames - 1) for index in indices])
+            logits += [*np.array(logits_states[0])[indices_to_fetch]]
+            states += [*np.array(list(map(lambda s: s.value, logits_states[1])))[indices_to_fetch]]
         return LogitDataset(logits, states)
 
     def videos_to_feature_maps_states(self, key: str):

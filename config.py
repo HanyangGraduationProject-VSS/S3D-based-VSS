c0a656911af36beab91560221122d2f3ffa83270
BraveSejin <ihavebravebiz@gmail.com>
2022-09-25 13:30:05 +0900

    update parquets path and make window states as bin:  Window.ACTIVITY, Window.NONE

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ default_window_size = 13
 
 H5DF_DIRPATH = path_join("/", "content", "drive", "MyDrive", "ActivityNet200")
 DATASET_DIRPATH = path_join("/", "content", "drive", "MyDrive", "ActivityNet200", "v1-3")
-parquets_path = path_join("/", "content", "drive", "MyDrive", "ActivityNet200", "parquets")
+parquets_path_train = path_join("/", "content", "drive", "MyDrive", "ActivityNet200", "parquet_train")
+parquets_path_test = path_join("/", "content", "drive", "MyDrive", "ActivityNet200", "parquet_test")
 
 # H5DF_DIRPATH = path_join(".")
 # DATASET_DIRPATH = path_join("/", "Users", "hgs", "activitynet1.3")
--- features_logits_loader.py
+++ features_logits_loader.py
@@ -3,7 +3,8 @@ from os.path import join as path_join
 import os
 import pandas as pd
 import numpy as np
-from config import parquets_path
+from config import parquets_path_train
+from config import parquets_path_test
 
 
 # feature_dirs = (dir for dir in os.listdir(path_feature)
@@ -14,9 +15,14 @@ from config import parquets_path
 #     for feature in os.listdir(dir):
 #         print(torch.load(path_join(dir,feature)))
     
-def load_feature_map_and_logits(video_key: str):
+def load_feature_map_and_logits(video_key: str, dataset: str):
     # path = path_join('.', 'features_logits', f'{video_key}.parquet')
-    path = path_join(parquets_path, f'{video_key}.parquet')
+    path = ""
+    if dataset == "train":
+        path = path_join(parquets_path_train, f'{video_key}.parquet')
+    else:
+        path = path_join(parquets_path_test, f'{video_key}.parquet')
+    
     parquet_pd = pd.read_parquet(path)
     parquet_pd.feature_map = \
         parquet_pd.feature_map.apply(lambda x : np.frombuffer(x,dtype = np.float32))
